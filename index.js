6fd67f4bef71fc83197017572006104f26f4f95e
mrudulabadake07 <mrudulabadake6@gmail.com>
2026-05-12 12:35:15 +0530

    Updated index.js

---
+++ index.js
@@ -0,0 +1,36 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const cvData = {
+        personal:{
+            name: "Mrudula Badake",
+            role: "Full stack Developer",
+            email: "badakemrudula2006@gmail.com",
+        },
+        education:[{
+            year: "S.Y",
+            degree: "B.tech",
+            institute: "Punyashlok Ahilyadevi Solapur University",
+        }],
+        projects:[{
+            title: "Interactive CV",
+            description: "websito to update cv whenever we want",
+            tech:["HTML","CSS","JS"]
+        }],
+        skills:["HTML", "CSS", "JS", "Java", "Python"],
+        experience:[{
+            title: "123",
+            comapany: "ABC",
+            year: "1234",
+            details:"sdkjfdsi shesi wirhesi"
+        }, {
+            title: "456",
+            comapny: "DEF",
+            year: "2020",
+            details:"sdkjfdsi shesi "
+            }],
+        lastUpdated:"DUMMY string",
+        updateHistory:[{
+            date: "11/2/2026",
+            summary:"sdlgj eijfei sifjsok"
+        }]
+    }
+})
